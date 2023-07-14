3be9fd7d1a23e6e7dd5ac2caf4dfe3c8b8e6ae3d
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-14 15:10:24 +0200

    init Bed.php containing Bed (Product subclass) with its controller

---
+++ Models/Bed.php
@@ -0,0 +1,12 @@
+<?php
+    class Toy extends Product {
+        public $material;
+        public $size;
+
+        function __construct(String $title, Float $price, String $description, String $barcode, String $brand, Float $rating, String $imageUrl, String $material, String $size) {
+            parent::__construct($title, $price, $description, $barcode, $brand, $rating, $imageUrl);
+            $this->material = $material;
+            $this->size = $size;
+        }
+    }
+?>

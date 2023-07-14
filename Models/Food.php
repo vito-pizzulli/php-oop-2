1278a140f3c57044bf85ba3d896123160fa01779
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-14 15:08:34 +0200

    small fix done in Food.php

--- Models/Food.php
+++ Models/Food.php
@@ -3,7 +3,7 @@
         public $type;
         public $taste;
         public $ingredients;
-        public $nutritional_values;
+        public $nutritionalValues;
 
         function __construct(String $title, Float $price, String $description, String $barcode, String $brand, Float $rating, String $imageUrl, String $type, String $taste, String $ingredients, String $nutritionalValues) {
             parent::__construct($title, $price, $description, $barcode, $brand, $rating, $imageUrl);

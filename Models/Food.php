1bffe5817a53aa46a943f476232e01af142ef13a
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-14 14:59:32 +0200

    added "barcode" instance variable to Product class

--- Models/Food.php
+++ Models/Food.php
@@ -5,8 +5,8 @@
         public $ingredients;
         public $nutritional_values;
 
-        function __construct(String $title, Float $price, String $description, String $imageUrl, String $type, String $taste, String $ingredients, String $nutritionalValues) {
-            parent::__construct($title, $price, $description, $imageUrl);
+        function __construct(String $title, Float $price, String $description, String $barcode, String $imageUrl, String $type, String $taste, String $ingredients, String $nutritionalValues) {
+            parent::__construct($title, $price, $description, $barcode, $imageUrl);
             $this->type = $type;
             $this->taste = $taste;
             $this->ingredients = $ingredients;
--- Models/Product.php
+++ Models/Product.php
@@ -3,12 +3,14 @@
         public $title;
         public $price;
         public $description;
+        public $barcode;
         public $imageUrl;
 
-        function __construct(String $title, Float $price, String $description, String $imageUrl) {
+        function __construct(String $title, Float $price, String $description, String $barcode, String $imageUrl) {
             $this->title = $title;
             $this->price = $price;
             $this->description = $description;
+            $this->barcode = $barcode;
             $this->imageUrl = $imageUrl;
         }
     }

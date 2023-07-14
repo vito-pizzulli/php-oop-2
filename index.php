af64b125e0da654963b3c00ffb4c238f3c80f68a
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-14 18:07:00 +0200

    added bootstrap & style.css, styling completed

--- index.php
+++ index.php
@@ -8,96 +8,111 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP OOP 2</title>
+    <link rel="stylesheet" href="./styles/style.css">
 
     <!-- FontAwesome 6.4.0 -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- Bootstrap 5.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 <body>
+    <header>
+        <div class="row">
+            <div class="col-12 text-center bg-warning p-3">
+                <h1>PHP OOP 2</h1>
+            </div>
+        </div>
+    </header>
     <main>
+        <div class="container d-flex flex-wrap">
         <?php
             foreach($dogProducts as $dogProduct) {
         ?>
-            <ul>
-                <li>Titolo: <?php echo $dogProduct->title ?></li>
-                <li>Prezzo: <?php printf('%.2f', $dogProduct->price) ?> €</li>
-                <li>Descrizione: <?php echo $dogProduct->description ?></li>
-                <li>Barcode: <?php echo $dogProduct->barcode ?></li>
-                <li>Marca: <?php echo $dogProduct->brand ?></li>
-                <li>Marca: <?php echo $dogProduct->rating ?></li>
-                <li><img src="<?php echo $dogProduct->imageUrl ?>" alt="Product Image"></li>
-                <li><i class="fa-solid fa-dog"></i></li>
-                <?php
-                    echo $dogProduct::$category;
+                <div class="card m-3" style="width: calc(100% / 3 - 2rem);">
+                    <img src="<?php echo $dogProduct->imageUrl ?>" class="card-img-top" alt="Product Image">
+                    <div class="card-body">
+                        <h2 class="card-title"><?php echo $dogProduct->title ?></h2>
+                        <p class="card-text"><?php echo $dogProduct->description ?></p>
+                        <span class="card-text">Barcode: <?php echo $dogProduct->barcode ?></span>
+                        <span class="card-text">Marca: <?php echo $dogProduct->brand ?></span>
+                        <span class="card-text">Valutazione: <?php echo $dogProduct->rating ?></span>
+                        <span><i class="fa-solid fa-dog"></i></span>
+                        <?php echo $dogProduct::$category;
 
-                    if ($dogProduct::$category == 'Cibo') {
-                        ?>
-                            <li><i class="fa-solid fa-drumstick-bite"></i></li>
-                            <li>Tipo: <?php echo $dogProduct->type ?></li>
-                            <li>Gusto: <?php echo $dogProduct->taste ?></li>
-                            <li>Ingredienti: <?php echo $dogProduct->ingredients ?></li>
-                            <li>Valori nutrizionali: <?php echo $dogProduct->nutritionalValues ?></li>
-                        <?php
-                    } elseif ($dogProduct::$category == 'Gioco') {
-                        ?>
-                            <li><i class="fa-solid fa-baseball"></i></li>
-                            <li>Materiale: <?php echo $dogProduct->material ?></li>
-                        <?php
-                    } elseif ($dogProduct::$category == 'Cuccia') {
-                        ?>
-                            <li><i class="fa-solid fa-moon"></i></li>
-                            <li>Materiale: <?php echo $dogProduct->material ?></li>
-                            <li>Dimensioni: <?php echo $dogProduct->size ?></li>
-                        <?php
-                    } else {
-                        ?><li><i class="fa-solid fa-tag"></i></li><?php
-                    }
-                ?>
-            </ul>
+                        if ($dogProduct::$category == 'Cibo') {
+                            ?>
+                                <span><i class="fa-solid fa-drumstick-bite"></i></span>
+                                <span>Tipo: <?php echo $dogProduct->type ?></span>
+                                <span>Gusto: <?php echo $dogProduct->taste ?></span>
+                                <p>Ingredienti: <?php echo $dogProduct->ingredients ?></p>
+                                <p>Valori nutrizionali: <?php echo $dogProduct->nutritionalValues ?></p>
+                            <?php
+                        } elseif ($dogProduct::$category == 'Gioco') {
+                            ?>
+                                <span><i class="fa-solid fa-baseball"></i></span>
+                                <span>Materiale: <?php echo $dogProduct->material ?></span>
+                            <?php
+                        } elseif ($dogProduct::$category == 'Cuccia') {
+                            ?>
+                                <span><i class="fa-solid fa-moon"></i></span>
+                                <span>Materiale: <?php echo $dogProduct->material ?></span>
+                                <span>Dimensioni: <?php echo $dogProduct->size ?></span>
+                            <?php
+                        } else {
+                            ?><span><i class="fa-solid fa-tag"></i></span><?php
+                        }
+                            ?>
+                            <h3 class="card-text text-success"><?php printf('%.2f', $dogProduct->price) ?> €</h3>
+                            <a href="#" class="btn btn-success">Acquista</a>
+                    </div>
+                </div>
         <?php
             }
-
             foreach($catProducts as $catProduct) {
-                ?>
-                    <ul>
-                        <li>Titolo: <?php echo $catProduct->title ?></li>
-                        <li>Prezzo: <?php printf('%.2f', $catProduct->price) ?> €</li>
-                        <li>Descrizione: <?php echo $catProduct->description ?></li>
-                        <li>Barcode: <?php echo $catProduct->barcode ?></li>
-                        <li>Marca: <?php echo $catProduct->brand ?></li>
-                        <li>Marca: <?php echo $catProduct->rating ?></li>
-                        <li><img src="<?php echo $catProduct->imageUrl ?>" alt="Product Image"></li>
-                        <li><i class="fa-solid fa-cat"></i></li>
-                        <?php
-                            echo $catProduct::$category;
-        
-                            if ($catProduct::$category == 'Cibo') {
-                                ?>
-                                    <li><i class="fa-solid fa-drumstick-bite"></i></li>
-                                    <li>Tipo: <?php echo $catProduct->type ?></li>
-                                    <li>Gusto: <?php echo $catProduct->taste ?></li>
-                                    <li>Ingredienti: <?php echo $catProduct->ingredients ?></li>
-                                    <li>Valori nutrizionali: <?php echo $catProduct->nutritionalValues ?></li>
-                                <?php
-                            } elseif ($catProduct::$category == 'Gioco') {
-                                ?>
-                                    <li><i class="fa-solid fa-baseball"></i></li>
-                                    <li>Materiale: <?php echo $catProduct->material ?></li>
-                                <?php
-                            } elseif ($catProduct::$category == 'Cuccia') {
-                                ?>
-                                    <li><i class="fa-solid fa-moon"></i></li>
-                                    <li>Materiale: <?php echo $catProduct->material ?></li>
-                                    <li>Dimensioni: <?php echo $catProduct->size ?></li>
-                                <?php
-                            } else {
-                                ?><li><i class="fa-solid fa-tag"></i></li><?php
-                            }
-                        ?>
-                    </ul>
-                <?php
-                    }
-                ?>
         ?>
+                <div class="card m-3" style="width: calc(100% / 3 - 2rem);">
+                    <img src="<?php echo $catProduct->imageUrl ?>" class="card-img-top" alt="Product Image">
+                    <div class="card-body">
+                        <h2 class="card-title"><?php echo $catProduct->title ?></h2>
+                        <p class="card-text"><?php echo $catProduct->description ?></p>
+                        <span class="card-text">Barcode: <?php echo $catProduct->barcode ?></span>
+                        <span class="card-text">Marca: <?php echo $catProduct->brand ?></span>
+                        <span class="card-text">Valutazione: <?php echo $catProduct->rating ?></span>
+                        <span><i class="fa-solid fa-cat"></i></span>
+                        <?php echo $catProduct::$category;
+
+                        if ($catProduct::$category == 'Cibo') {
+                            ?>
+                                <span><i class="fa-solid fa-drumstick-bite"></i></span>
+                                <span>Tipo: <?php echo $catProduct->type ?></span>
+                                <span>Gusto: <?php echo $catProduct->taste ?></span>
+                                <p>Ingredienti: <?php echo $catProduct->ingredients ?></p>
+                                <p>Valori nutrizionali: <?php echo $catProduct->nutritionalValues ?></p>
+                            <?php
+                        } elseif ($catProduct::$category == 'Gioco') {
+                            ?>
+                                <span><i class="fa-solid fa-baseball"></i></span>
+                                <span>Materiale: <?php echo $catProduct->material ?></span>
+                            <?php
+                        } elseif ($catProduct::$category == 'Cuccia') {
+                            ?>
+                                <span><i class="fa-solid fa-moon"></i></span>
+                                <span>Materiale: <?php echo $catProduct->material ?></span>
+                                <span>Dimensioni: <?php echo $catProduct->size ?></span>
+                            <?php
+                        } else {
+                            ?><span><i class="fa-solid fa-tag"></i></span><?php
+                        }
+                            ?>
+                            <h3 class="card-text text-success"><?php printf('%.2f', $catProduct->price) ?> €</h3>
+                            <a href="#" class="btn btn-success">Acquista</a>
+                    </div>
+                </div>
+        <?php
+            }
+        ?>
+        </div>
     </main>
 </body>
 </html>
